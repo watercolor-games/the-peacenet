1257ce76362c8df9a73c9fcd44aa48c6e8cb2bea
Michael <Michael@some.where>
2017-01-18 21:07:22 -0500

    Add simple tasks for jobs.

---
+++ ShiftOS.WinForms/JobTasks.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShiftOS.Objects;
+using ShiftOS.Engine;
+
+
+namespace ShiftOS.WinForms
+{
+    public class AcquireCodepointsJobTask : JobTask
+    {
+        public AcquireCodepointsJobTask(int amount)
+        {
+            CodepointsRequired = SaveSystem.CurrentSave.Codepoints + amount;
+        }
+
+        public int CodepointsRequired { get; private set; }
+
+        public override bool IsComplete
+        {
+            get
+            {
+                return (SaveSystem.CurrentSave.Codepoints >= CodepointsRequired);
+            }
+        }
+    }
+
+    public class AcquireUpgradeJobTask : JobTask
+    {
+        public AcquireUpgradeJobTask(string upgId)
+        {
+            UpgradeID = upgId;
+        }
+
+        public string UpgradeID { get; private set; }
+
+        public override bool IsComplete
+        {
+            get
+            {
+                return Shiftorium.UpgradeInstalled(UpgradeID);
+            }
+        }
+    }
+}
