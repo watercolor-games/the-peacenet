4ec9ca6eca5c9f3bce6d9fd66dcf118f8e5ce73c
Michael <Michael@some.where>
2017-02-11 12:27:35 -0500

    commit not safe for children

--- ShiftOS.WinForms/Properties/AssemblyInfo.cs
+++ ShiftOS.WinForms/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ShiftOS.WinForms")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Your god damn motherfucking piece of shit fucking game")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ShiftOS.WinForms")]
