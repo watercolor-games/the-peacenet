a3c1c1d945affd76e610060459b5ab7a3a10b42b
william341 <wwokwilliam@gmail.com>
2017-02-11 10:29:59 -0700

    Revert "commit not safe for children"
    This reverts commit 4ec9ca6eca5c9f3bce6d9fd66dcf118f8e5ce73c.

--- ShiftOS.WinForms/Properties/AssemblyInfo.cs
+++ ShiftOS.WinForms/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ShiftOS.WinForms")]
-[assembly: AssemblyDescription("Your god damn motherfucking piece of shit fucking game")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ShiftOS.WinForms")]
