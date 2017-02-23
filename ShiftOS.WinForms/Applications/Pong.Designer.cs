bd051072ff58f76983741b24d752945439937c26
craftxbox <craftxbox@users.noreply.github.com>
2017-02-23 19:07:21 -0330

    NOT BE AN IDIOT

--- ShiftOS.WinForms/Applications/Pong.Designer.cs
+++ ShiftOS.WinForms/Applications/Pong.Designer.cs
@@ -1,21 +1,4 @@
-Skip to content
-This repository
-Search
-Pull requests
-Issues
-Gist
- @craftxbox
- Sign out
- Watch 5
-  Star 6
-  Fork 9 shiftos-game/ShiftOS
- Code  Issues 7  Pull requests 0  Projects 0  Wiki  Pulse  Graphs
-Branch: master Find file Copy pathShiftOS/ShiftOS.WinForms/Applications/Pong.Designer.cs
-6f939e2  12 days ago
- Michael Fix Pong UI and panel buttons
-0 contributors
-RawBlameHistory     
-718 lines (713 sloc)  39.2 KB
+
 /*
  * MIT License
  * 
@@ -571,11 +554,14 @@ namespace ShiftOS.WinForms.Applications
             this.paddleHuman.BackColor = System.Drawing.Color.Black;
             this.paddleHuman.Location = new System.Drawing.Point(10, 134);
             this.paddleHuman.Name = "paddleHuman";
-			if(ShiftoriumFrontend.UpgradeInstalled("pong_increased_paddle_size")){
-				this.paddleHuman.Size = new System.Drawing.Size(20, 150);
-			} else {
-				this.paddleHuman.Size = new System.Drawing.Size(20, 100);
-			}
+            if (ShiftoriumFrontend.UpgradeInstalled("pong_increased_paddle_size"))
+            {
+                this.paddleHuman.Size = new System.Drawing.Size(20, 150);
+            }
+            else
+            {
+                this.paddleHuman.Size = new System.Drawing.Size(20, 100);
+            }
             this.paddleHuman.TabIndex = 3;
             this.paddleHuman.TabStop = false;
             // 
@@ -586,11 +572,14 @@ namespace ShiftOS.WinForms.Applications
             this.paddleComputer.Location = new System.Drawing.Point(666, 134);
             this.paddleComputer.MaximumSize = new System.Drawing.Size(20, 100);
             this.paddleComputer.Name = "paddleComputer";
-			if(ShiftoriumFrontend.UpgradeInstalled("pong_increased_paddle_size")){
-				this.paddleComputer.Size = new System.Drawing.Size(20, 150);
-			} else {
-				this.paddleComputer.Size = new System.Drawing.Size(20, 100);
-			}
+            if (ShiftoriumFrontend.UpgradeInstalled("pong_increased_paddle_size"))
+            {
+                this.paddleComputer.Size = new System.Drawing.Size(20, 150);
+            }
+            else
+            {
+                this.paddleComputer.Size = new System.Drawing.Size(20, 100);
+            }
             this.paddleComputer.TabIndex = 1;
             // 
             // lbllevelandtime
@@ -606,7 +595,7 @@ namespace ShiftOS.WinForms.Applications
             // 
             // lblstatscodepoints
             // 
-            this.lblstatscodepoints.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.lblstatscodepoints.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lblstatscodepoints.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblstatscodepoints.Location = new System.Drawing.Point(239, 356);
