4ff53eb4a271ee7ee3bc52674a29fb6b2d07e275
lempamo <smartypantslf@gmail.com>
2017-02-24 20:12:14 -0500

    Get started on Calculator

---
+++ ShiftOS.WinForms/Applications/Calculator.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace ShiftOS.WinForms.Applications
+{
+    partial class Calculator
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // numBox
+            // 
+            this.numBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numBox.Location = new System.Drawing.Point(4, 3);
+            this.numBox.Name = "numBox";
+            this.numBox.Size = new System.Drawing.Size(143, 30);
+            this.numBox.TabIndex = 0;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.numBox);
+            this.Name = "Calculator";
+            this.Load += new System.EventHandler(this.Template_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox numBox;
+    }
+}
---
+++ ShiftOS.WinForms/Applications/Calculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ShiftOS.Engine;
+
+namespace ShiftOS.WinForms.Applications
+{
+    [Launcher("Calculator", true, "al_calculator", "Accessories")]
+    [RequiresUpgrade("calculator")]
+    [WinOpen("calculator")]
+    [DefaultIcon("iconCalculator")]
+    public partial class Calculator : UserControl, IShiftOSWindow
+    {
+        public bool justopened = false;
+
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Template_Load(object sender, EventArgs e)
+        {
+            justopened = true;
+        }
+
+        public void OnLoad()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnSkinLoad()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OnUnload()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnUpgrade()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
