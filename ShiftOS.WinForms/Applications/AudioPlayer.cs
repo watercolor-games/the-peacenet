84abd2cd11b5bcd78052015cc1f33b9e738b7417
lempamo <smartypantslf@gmail.com>
2017-02-25 16:10:59 -0500

    Basic AudioPlayer
    planning to use naudio api for this. thoughts?

---
+++ ShiftOS.WinForms/Applications/AudioPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ShiftOS.Engine;
+
+namespace ShiftOS.WinForms.Applications
+{
+    public partial class AudioPlayer : UserControl, IShiftOSWindow
+    {
+        public AudioPlayer()
+        {
+            InitializeComponent();
+        }
+
+        public void OnLoad()
+        {
+            
+        }
+
+        public void OnSkinLoad()
+        {
+            
+        }
+
+        public bool OnUnload()
+        {
+            return true;
+        }
+
+        public void OnUpgrade()
+        {
+            
+        }
+    }
+}
