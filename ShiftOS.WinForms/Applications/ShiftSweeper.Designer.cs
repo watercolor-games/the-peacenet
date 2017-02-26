f765e3ef33129f6a21641f7ccdb667ea157d7eed
lempamo <smartypantslf@gmail.com>
2017-02-26 12:09:52 -0500

    ss things 2

--- ShiftOS.WinForms/Applications/ShiftSweeper.Designer.cs
+++ ShiftOS.WinForms/Applications/ShiftSweeper.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonE = new System.Windows.Forms.Button();
+            this.buttonM = new System.Windows.Forms.Button();
+            this.buttonH = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,13 +46,57 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(4, 39);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(244, 215);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // buttonE
+            // 
+            this.buttonE.Location = new System.Drawing.Point(4, 261);
+            this.buttonE.Name = "buttonE";
+            this.buttonE.Size = new System.Drawing.Size(75, 23);
+            this.buttonE.TabIndex = 2;
+            this.buttonE.Text = "Easy";
+            this.buttonE.UseVisualStyleBackColor = true;
+            // 
+            // buttonM
+            // 
+            this.buttonM.Location = new System.Drawing.Point(86, 261);
+            this.buttonM.Name = "buttonM";
+            this.buttonM.Size = new System.Drawing.Size(75, 23);
+            this.buttonM.TabIndex = 3;
+            this.buttonM.Text = "Medium";
+            this.buttonM.UseVisualStyleBackColor = true;
+            // 
+            // buttonH
+            // 
+            this.buttonH.Location = new System.Drawing.Point(168, 261);
+            this.buttonH.Name = "buttonH";
+            this.buttonH.Size = new System.Drawing.Size(75, 23);
+            this.buttonH.TabIndex = 4;
+            this.buttonH.Text = "Hard";
+            this.buttonH.UseVisualStyleBackColor = true;
+            // 
             // ShiftSweeper
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.buttonH);
+            this.Controls.Add(this.buttonM);
+            this.Controls.Add(this.buttonE);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "ShiftSweeper";
-            this.Size = new System.Drawing.Size(270, 333);
+            this.Size = new System.Drawing.Size(275, 333);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -57,5 +105,9 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonE;
+        private System.Windows.Forms.Button buttonM;
+        private System.Windows.Forms.Button buttonH;
     }
 }
