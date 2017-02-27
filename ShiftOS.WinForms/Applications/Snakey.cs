43364450a2215a92000d4fa62c11e30bf1a3d295
lempamo <smartypantslf@gmail.com>
2017-02-27 12:08:50 -0500

    Snakey basics

---
+++ ShiftOS.WinForms/Applications/Snakey.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ShiftOS.Engine;
+
+namespace ShiftOS.WinForms.Applications
+{
+    [Launcher("Snakey", true, "al_snakey", "Games")]
+    [RequiresUpgrade("snakey")]
+    [WinOpen("snakey")]
+    [DefaultIcon("iconSnakey")]
+    public partial class Snakey : UserControl, IShiftOSWindow
+    {
+        public Snakey()
+        {
+            InitializeComponent();
+        }
+
+        public void OnLoad()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnSkinLoad()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OnUnload()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnUpgrade()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
