c1567f93f3a0bad3f8ef984bf980666cb31dfd87
lempamo <smartypantslf@gmail.com>
2017-03-06 09:31:07 -0500

    extremely minor chaange

--- ShiftOS.WinForms/Applications/Chat.cs
+++ ShiftOS.WinForms/Applications/Chat.cs
@@ -55,7 +55,7 @@ namespace ShiftOS.WinForms.Applications
                                 var args = JsonConvert.DeserializeObject<Dictionary<string, string>>(msg.Contents);
                                 var cmsg = new ShiftOS.Objects.ChatMessage(args["Username"] as string, args["SystemName"] as string, args["Message"] as string, args["Channel"] as string);
                                 if (id == cmsg.Channel)
-                                    rtbchat.AppendText($"[{cmsg.Username}@{cmsg.SystemName}] {cmsg.Message}{Environment.NewLine}");
+                                    rtbchat.AppendText($"[{cmsg.Username}@{cmsg.SystemName}]: {cmsg.Message}{Environment.NewLine}");
                             }
                             catch (Exception ex)
                             {
