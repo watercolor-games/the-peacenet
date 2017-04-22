e65a1b5e93e8f533d44542354b150ac660cf7239
Michael <Michael@some.where>
2017-04-22 18:57:05 -0400

    BST: Add "CommandRunner" Widget

---
+++ ShiftOS.WinForms/DesktopWidgets/CommandRunner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ShiftOS.Engine;
+using ShiftOS.WinForms.Tools;
+
+namespace ShiftOS.WinForms.DesktopWidgets
+{
+    [DesktopWidget("Terminal Widget", "Run commands in a miniature Terminal.")]
+    public partial class CommandRunner : UserControl, IDesktopWidget
+    {
+        public CommandRunner()
+        {
+            InitializeComponent();
+        }
+
+        public void OnSkinLoad()
+        {
+            ControlManager.SetupControls(this);
+        }
+
+        public void OnUpgrade()
+        {
+
+        }
+
+        public void Setup()
+        {
+        }
+    }
+}
