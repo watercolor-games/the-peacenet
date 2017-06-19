45c587b3a656135fec9c91566fa5f817ddfc600e
Michael <Michael@some.where>
2017-06-18 20:53:03 -0400

    Localize BIOS screen.

--- ShiftOS.WinForms/MainMenu/Loading.cs
+++ ShiftOS.WinForms/MainMenu/Loading.cs
@@ -16,8 +16,23 @@ namespace ShiftOS.WinForms.MainMenu
         public Loading()
         {
             InitializeComponent();
-            label.Text = Localization.Parse(label.Text);
-            label1.Text = "AMIBIOS(C)"+ DateTime.Now.Year +" Australian Microtrends, Plc.\nShiftsoft 480-L ACPI BIOS Revision 1002\nCPU : VTC 210-N " + Environment.ProcessorCount + " CPU 1.33GHz\n   Speed : 1.337Ghz\n\nPress DEL to run Setup\nPress <F8> for BBS POPUP\nDDR3 Frequency 1337MHz, Dual Channel, Linear Mode\nChecking NVRAM\n\n1337MB OK";
+            
+            //Now let's do it Michael-style.
+            label1.Text = Localization.Parse(@"{MISC_BIOSCOPYRIGHT}
+{MISC_BIOSVERSION}
+{MISC_BIOSCPU}
+{MISC_CLOCKSPEED}
+
+{MISC_RUNSETUP}
+{MISC_BBSPOPUP}
+{MISC_RAMFREQ}
+{MISC_CHECKINGNVRAM}
+
+{MISC_RAM}", new Dictionary<string, string>
+            {
+                                                 ["%year"] = DateTime.Now.Year.ToString(),
+                                                 ["%cores"] = Environment.ProcessorCount.ToString()
+            });
         }
 
         private void Loading_FormShown(object sender, EventArgs e)
