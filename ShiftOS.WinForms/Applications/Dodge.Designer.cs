027788fa853aab3f41b1115ac68c6635f9975252
RogueAI42 <declanhoare@exemail.com.au>
2017-06-29 21:18:34 +1000

    make dodge exist again
    kbd input doesn't work at all, the collision detection sucks, and
    if you maximise the window the game instantly becomes easy, but
    it's a start.

---
+++ ShiftOS.WinForms/Applications/Dodge.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace ShiftOS.WinForms.Applications
+{
+    partial class Dodge
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dodge));
+            this.pgcontents = new System.Windows.Forms.Panel();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.QuitButton = new System.Windows.Forms.PictureBox();
+            this.BeginButton = new System.Windows.Forms.PictureBox();
+            this.DescriptionLabel = new System.Windows.Forms.Label();
+            this.object_small2 = new System.Windows.Forms.PictureBox();
+            this.object_mid2 = new System.Windows.Forms.PictureBox();
+            this.object_large = new System.Windows.Forms.PictureBox();
+            this.object_small = new System.Windows.Forms.PictureBox();
+            this.object_mid = new System.Windows.Forms.PictureBox();
+            this.scorelabel = new System.Windows.Forms.Label();
+            this.PicBonus = new System.Windows.Forms.PictureBox();
+            this.clock = new System.Windows.Forms.Timer(this.components);
+            this.pgcontents.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.QuitButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BeginButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.object_small2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.object_mid2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.object_large)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.object_small)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.object_mid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBonus)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pgcontents
+            // 
+            this.pgcontents.BackColor = System.Drawing.Color.White;
+            this.pgcontents.Controls.Add(this.player);
+            this.pgcontents.Controls.Add(this.QuitButton);
+            this.pgcontents.Controls.Add(this.BeginButton);
+            this.pgcontents.Controls.Add(this.DescriptionLabel);
+            this.pgcontents.Controls.Add(this.object_small2);
+            this.pgcontents.Controls.Add(this.object_mid2);
+            this.pgcontents.Controls.Add(this.object_large);
+            this.pgcontents.Controls.Add(this.object_small);
+            this.pgcontents.Controls.Add(this.object_mid);
+            this.pgcontents.Controls.Add(this.scorelabel);
+            this.pgcontents.Controls.Add(this.PicBonus);
+            this.pgcontents.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pgcontents.Location = new System.Drawing.Point(0, 0);
+            this.pgcontents.Name = "pgcontents";
+            this.pgcontents.Size = new System.Drawing.Size(597, 567);
+            this.pgcontents.TabIndex = 21;
+            this.pgcontents.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pgcontents_MouseMove);
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = ((System.Drawing.Image)(resources.GetObject("player.Image")));
+            this.player.Location = new System.Drawing.Point(192, 445);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(32, 32);
+            this.player.TabIndex = 18;
+            this.player.TabStop = false;
+            // 
+            // QuitButton
+            // 
+            this.QuitButton.Image = ((System.Drawing.Image)(resources.GetObject("QuitButton.Image")));
+            this.QuitButton.Location = new System.Drawing.Point(216, 424);
+            this.QuitButton.Name = "QuitButton";
+            this.QuitButton.Size = new System.Drawing.Size(200, 50);
+            this.QuitButton.TabIndex = 12;
+            this.QuitButton.TabStop = false;
+            this.QuitButton.Click += new System.EventHandler(this.QuitButton_Click);
+            // 
+            // BeginButton
+            // 
+            this.BeginButton.Image = ((System.Drawing.Image)(resources.GetObject("BeginButton.Image")));
+            this.BeginButton.Location = new System.Drawing.Point(3, 424);
+            this.BeginButton.Name = "BeginButton";
+            this.BeginButton.Size = new System.Drawing.Size(200, 50);
+            this.BeginButton.TabIndex = 11;
+            this.BeginButton.TabStop = false;
+            this.BeginButton.Click += new System.EventHandler(this.BeginButton_Click);
+            // 
+            // DescriptionLabel
+            // 
+            this.DescriptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DescriptionLabel.Location = new System.Drawing.Point(3, 3);
+            this.DescriptionLabel.Name = "DescriptionLabel";
+            this.DescriptionLabel.Size = new System.Drawing.Size(413, 409);
+            this.DescriptionLabel.TabIndex = 10;
+            this.DescriptionLabel.Text = "Placeholder";
+            // 
+            // object_small2
+            // 
+            this.object_small2.Image = ((System.Drawing.Image)(resources.GetObject("object_small2.Image")));
+            this.object_small2.Location = new System.Drawing.Point(75, 43);
+            this.object_small2.Name = "object_small2";
+            this.object_small2.Size = new System.Drawing.Size(75, 20);
+            this.object_small2.TabIndex = 17;
+            this.object_small2.TabStop = false;
+            // 
+            // object_mid2
+            // 
+            this.object_mid2.Image = ((System.Drawing.Image)(resources.GetObject("object_mid2.Image")));
+            this.object_mid2.Location = new System.Drawing.Point(279, 134);
+            this.object_mid2.Name = "object_mid2";
+            this.object_mid2.Size = new System.Drawing.Size(125, 20);
+            this.object_mid2.TabIndex = 16;
+            this.object_mid2.TabStop = false;
+            // 
+            // object_large
+            // 
+            this.object_large.Image = ((System.Drawing.Image)(resources.GetObject("object_large.Image")));
+            this.object_large.Location = new System.Drawing.Point(49, 208);
+            this.object_large.Name = "object_large";
+            this.object_large.Size = new System.Drawing.Size(175, 20);
+            this.object_large.TabIndex = 15;
+            this.object_large.TabStop = false;
+            // 
+            // object_small
+            // 
+            this.object_small.Image = ((System.Drawing.Image)(resources.GetObject("object_small.Image")));
+            this.object_small.Location = new System.Drawing.Point(290, 294);
+            this.object_small.Name = "object_small";
+            this.object_small.Size = new System.Drawing.Size(75, 20);
+            this.object_small.TabIndex = 13;
+            this.object_small.TabStop = false;
+            // 
+            // object_mid
+            // 
+            this.object_mid.Image = ((System.Drawing.Image)(resources.GetObject("object_mid.Image")));
+            this.object_mid.Location = new System.Drawing.Point(58, 371);
+            this.object_mid.Name = "object_mid";
+            this.object_mid.Size = new System.Drawing.Size(125, 20);
+            this.object_mid.TabIndex = 14;
+            this.object_mid.TabStop = false;
+            // 
+            // scorelabel
+            // 
+            this.scorelabel.AutoSize = true;
+            this.scorelabel.BackColor = System.Drawing.Color.Transparent;
+            this.scorelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scorelabel.Location = new System.Drawing.Point(3, 4);
+            this.scorelabel.Name = "scorelabel";
+            this.scorelabel.Size = new System.Drawing.Size(51, 55);
+            this.scorelabel.TabIndex = 19;
+            this.scorelabel.Text = "0";
+            // 
+            // PicBonus
+            // 
+            this.PicBonus.Image = ((System.Drawing.Image)(resources.GetObject("PicBonus.Image")));
+            this.PicBonus.Location = new System.Drawing.Point(187, 84);
+            this.PicBonus.Name = "PicBonus";
+            this.PicBonus.Size = new System.Drawing.Size(16, 11);
+            this.PicBonus.TabIndex = 20;
+            this.PicBonus.TabStop = false;
+            this.PicBonus.Visible = false;
+            // 
+            // clock
+            // 
+            this.clock.Interval = 20;
+            this.clock.Tick += new System.EventHandler(this.clock_Tick);
+            // 
+            // Dodge
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pgcontents);
+            this.Name = "Dodge";
+            this.Size = new System.Drawing.Size(597, 567);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Dodge_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Dodge_KeyUp);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Dodge_MouseMove);
+            this.pgcontents.ResumeLayout(false);
+            this.pgcontents.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.QuitButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BeginButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.object_small2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.object_mid2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.object_large)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.object_small)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.object_mid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBonus)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Panel pgcontents;
+        internal System.Windows.Forms.PictureBox player;
+        internal System.Windows.Forms.PictureBox QuitButton;
+        internal System.Windows.Forms.PictureBox BeginButton;
+        internal System.Windows.Forms.Label DescriptionLabel;
+        internal System.Windows.Forms.PictureBox object_small2;
+        internal System.Windows.Forms.PictureBox object_mid2;
+        internal System.Windows.Forms.PictureBox object_large;
+        internal System.Windows.Forms.PictureBox object_small;
+        internal System.Windows.Forms.PictureBox object_mid;
+        internal System.Windows.Forms.Label scorelabel;
+        internal System.Windows.Forms.PictureBox PicBonus;
+        internal System.Windows.Forms.Timer clock;
+    }
+}
---
+++ ShiftOS.WinForms/Applications/Dodge.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ShiftOS.Engine;
+using Microsoft.VisualBasic;
+
+namespace ShiftOS.WinForms.Applications
+{
+    [AppscapeEntry("dodge", "Dodge", "Dodge falling objects and stay alive as long as you can. Play this fun and exciting game while earning code points!", 500, 1590, "", "Games")]
+    [WinOpen("dodge")]
+    [Launcher("Dodge", false, "", "Games")]
+    public partial class Dodge : UserControl, IShiftOSWindow
+    {
+        decimal speed; //the speed the game runs at
+        int score; //the score/code points the player gets
+        bool usingkeys = false; //user can use mouse or keyboard, mouse by default, chnages to true if key pressed
+        decimal time = 0; //Records the time spent playing, used for codepoints formula
+        int bonusesfound; //Number or bonus play collects
+        int keyboardinput = 0; //for smooth keyboard gameplay, 1=left, 2=right, 0=none
+        Point LocalMousePosition = new Point(0, 0); // the position of the mouse relative to our control
+        public Dodge()
+        {
+            InitializeComponent();
+        }
+
+        public void OnSkinLoad()
+        {
+        }
+
+        public bool OnUnload()
+        {
+            return true;
+        }
+
+        public void OnUpgrade()
+        {
+        }
+
+        private void QuitButton_Click(object sender, EventArgs e)
+        {
+            AppearanceManager.Close(this); //quits the game (In case user donsn't have close button)
+        }
+
+        private void Dodge_KeyUp(object sender, KeyEventArgs e)
+        {
+            keyboardinput = 0;
+        }
+
+        public void OnLoad()
+        {
+            player.Visible = false; // hide player and score until game starts
+            scorelabel.Visible = false;
+            DescriptionLabel.Text = "Welcome to Dodge. Dodge is a simple arcade game with one objective: survive the falling objects! Use the arrow or mouse to move the player and avoid as many objects as you can. The longer you survive, the more code point you will be rewarded with. Beware, it gets harder..."; // set the description text
+
+            //to impliment skinning, simply set the picturebox to the new skinned image.
+            //For example:
+            //player.Image = Image.FromFile("PATH TO SKINNED IMAGE");
+        }
+
+        private void BeginButton_Click(object sender, EventArgs e)
+        {
+
+            //Hide buttons
+            BeginButton.Visible = false;
+            QuitButton.Visible = false;
+            DescriptionLabel.Visible = false;
+
+            player.Visible = true; // show the player
+            speed = 2; // controls speed of game, will increase as game progresses
+            scorelabel.Visible = true; // show score label
+            bonusesfound = 0;
+
+            //Make sure all objects are in the correct position
+            object_small.Location = new Point(((int)Math.Ceiling(VBMath.Rnd() * pgcontents.Width)), 300);
+            object_small2.Location = new Point(((int)Math.Ceiling(VBMath.Rnd() * pgcontents.Width)), 49);
+            object_mid.Location = new Point(((int)Math.Ceiling(VBMath.Rnd() * pgcontents.Width)), 377);
+            object_mid2.Location = new Point(((int)Math.Ceiling(VBMath.Rnd() * pgcontents.Width)), 140);
+            object_large.Location = new Point(((int)Math.Ceiling(VBMath.Rnd() * pgcontents.Width)), 214);
+            PicBonus.Location = new Point(((int)Math.Ceiling(VBMath.Rnd() * pgcontents.Width)), -20);
+
+            //Reset time
+            time = 0;
+
+            usingkeys = false;
+
+            System.Threading.Thread.Sleep(100); // slight delay before game starts (in milliseconds)
+
+            main(); // start the main game function
+
+            //sig() //infobox sigs -COMMENT THIS OUT
+        }
+
+        private void Dodge_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right: // detect right key press
+                    usingkeys = true; // turn off mouse control
+                    keyboardinput = 2;
+                    break;
+                case Keys.Left:
+                    usingkeys = true;
+                    keyboardinput = 1;
+                    break;
+            }
+
+        }
+
+        private void main()
+        {
+            clock.Start(); //the timer restart this sub every tick, making an endless loop between them.
+
+            //score system
+            scorelabel.Text = score.ToString();
+            score = (int)((speed / 10) + (time / 20) + bonusesfound);
+
+            //Speed increase
+            speed = speed + (speed * (decimal)0.001);
+
+            //increase time
+            time = time + (decimal)0.05; //loops every 0.05 seconds so time increases by 1 every second (have I done the maths correctly?)
+
+            //Make objects fall
+            object_large.Location = new Point(object_large.Location.X, (int)(object_large.Location.Y + speed));
+            object_mid.Location = new Point(object_mid.Location.X, (int)(object_mid.Location.Y + speed));
+            object_mid2.Location = new Point(object_mid2.Location.X, (int)(object_mid2.Location.Y + speed));
+            object_small.Location = new Point(object_small.Location.X, (int)(object_small.Location.Y + speed));
+            object_small2.Location = new Point(object_small2.Location.X, (int)(object_small2.Location.Y + speed));
+
+            //mouse controls
+            if (!usingkeys) // tests if mouse control is enabled
+            {
+                player.Left = LocalMousePosition.X - (player.Width / 2) - 5; //sets the x location to that of the mouse
+            }
+            //keyboard controls
+            if (usingkeys)
+            {
+                if (keyboardinput == 1)
+                    player.Location = new Point((int)(player.Location.X - (speed * 4)), player.Location.Y);
+                if (keyboardinput == 2)
+                    player.Location = new Point((int)(player.Location.X + (speed * 4)), player.Location.Y); // move right
+            }
+
+            //move object back to the top of the screen
+            if (object_small.Location.Y > 522)
+                object_small.Location = new Point((int)(Math.Ceiling(VBMath.Rnd() * pgcontents.Width)), -20); //picks a random number between 0 and the window width and sets the x position to this value. uses -20 for y as it is above the top of window
+            if (object_small2.Location.Y > 522)
+                object_small2.Location = new Point((int)(Math.Ceiling(VBMath.Rnd() * pgcontents.Width)), -20);
+            if (object_mid.Location.Y > 522)
+                object_mid.Location = new Point((int)(Math.Ceiling(VBMath.Rnd() * pgcontents.Width)), -20);
+            if (object_mid2.Location.Y > 522)
+                object_mid2.Location = new Point((int)(Math.Ceiling(VBMath.Rnd() * pgcontents.Width)), -20);
+            if (object_large.Location.Y > 522)
+                object_large.Location = new Point((int)(Math.Ceiling(VBMath.Rnd() * pgcontents.Width)), -20);
+
+            //Makes sure the player is on the screen (Anti-cheating)
+            if (player.Location.X > pgcontents.Width)
+                player.Location = new Point(385, player.Location.Y);
+            if (player.Location.X < 0)
+                player.Location = new Point(0, player.Location.Y);
+
+            //Bonus
+            if (!PicBonus.Visible)
+            {
+                int ran = (int)Math.Ceiling(VBMath.Rnd() * 300); //random 1 in 500 chance
+                if (ran == 1)
+                    PicBonus.Visible = true;
+            }
+            else
+            {
+                PicBonus.Location = new Point(PicBonus.Location.X, (int)(PicBonus.Location.Y + speed));
+                if (PicBonus.Location.Y > 522)
+                    PicBonus.Location = new Point((int)(Math.Ceiling(VBMath.Rnd() * pgcontents.Width)), -20);
+                PicBonus.Visible = false;
+            }
+
+            //check collisions
+            if (player.Bounds.IntersectsWith(object_mid.Bounds) || player.Bounds.IntersectsWith(object_mid2.Bounds) || player.Bounds.IntersectsWith(object_large.Bounds) || player.Bounds.IntersectsWith(object_small.Bounds) || player.Bounds.IntersectsWith(object_small2.Bounds))
+            {
+                clock.Stop(); //breaks loop
+                System.Threading.Thread.Sleep(333); //delay for a third of a second
+                player.Visible = false; //hide game
+                DescriptionLabel.Text = "Sorry, you just lost the game, however, you earnt a total of " + score.ToString() + " code points. To earn more code points, press the begin button now. To exit, press the quit button"; // change the description to the die message
+                SaveSystem.CurrentSave.Codepoints += (ulong) score;
+                DescriptionLabel.Visible = true; //show non-game elements
+                BeginButton.Visible = true;
+                QuitButton.Visible = true;
+                scorelabel.Visible = false;
+            }
+            if (player.Bounds.IntersectsWith(PicBonus.Bounds))
+            {
+                PicBonus.Visible = false;
+                bonusesfound = bonusesfound + 1;
+                PicBonus.Location = new Point((int)(Math.Ceiling(VBMath.Rnd() * pgcontents.Width)), -20);
+            }
+
+        }
+        private void clock_Tick(object sender, EventArgs e)
+        {
+            main(); //repeat the main function (endless loop)
+        }
+
+        private void sig()
+        {
+            Infobox.Show("FLAG", "There is no foul on the play, the punt was blocked.");
+        }
+
+        private void Dodge_MouseMove(object sender, MouseEventArgs e)
+        {
+            LocalMousePosition = e.Location;
+        }
+
+        private void pgcontents_MouseMove(object sender, MouseEventArgs e)
+        {
+            LocalMousePosition = e.Location;
+        }
+    }
+}
