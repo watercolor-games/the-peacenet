ae293c61acbfde26618da14605a985fc00f46677
Michael <Michael@some.where>
2017-06-28 21:48:03 -0400

    Codepoint Leech Virus
    Suggested by Rylan.

---
+++ ShiftOS.WinForms/Viruses/CPLeach.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShiftOS.Engine;
+
+namespace ShiftOS.WinForms.Viruses
+{
+    public class CPLeach : IVirus
+    {
+        public System.Windows.Forms.Timer Timer = null;
+
+        public void Infect(int threatlevel)
+        {
+            Timer = new System.Windows.Forms.Timer();
+            Timer.Interval = 6000;
+            Timer.Tick += (o, a) =>
+            {
+                ulong codepointDecrease = (ulong)threatlevel * 4;
+                if (SaveSystem.CurrentSave.Codepoints >= codepointDecrease)
+                    SaveSystem.CurrentSave.Codepoints -= codepointDecrease;
+                else
+                    SaveSystem.CurrentSave.Codepoints = 0;
+            };
+            Timer.Start();
+        }
+
+        public void Disinfect()
+        {
+            Timer.Stop();
+        }
+    }
+}
