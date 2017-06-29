95086532dcebbe7aeb3b900364afc5b3146e0910
Michael <Michael@some.where>
2017-06-28 21:53:47 -0400

    make the virus accessible

--- ShiftOS.WinForms/Viruses/CPLeach.cs
+++ ShiftOS.WinForms/Viruses/CPLeach.cs
@@ -7,6 +7,7 @@ using ShiftOS.Engine;
 
 namespace ShiftOS.WinForms.Viruses
 {
+    [Virus("cpleach", "Codepoint Leach", "Every 6 seconds, a certain amount of Codepoints is taken from the user until they're left with nothing. The amount taken away each time is determined by the threat level.")]
     public class CPLeach : IVirus
     {
         public System.Windows.Forms.Timer Timer = null;
