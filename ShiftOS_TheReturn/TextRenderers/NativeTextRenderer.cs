9454d57fe8802890d20c7b5697d5331e782c30e2
RogueAI42 <declanhoare@exemail.com.au>
2017-10-15 01:01:13 +1100

    revert the god forbid stuff

--- ShiftOS_TheReturn/TextRenderers/NativeTextRenderer.cs
+++ ShiftOS_TheReturn/TextRenderers/NativeTextRenderer.cs
@@ -23,42 +23,17 @@ namespace Plex.Engine.TextRenderers
 				Words = 2
 			}
 		}
-		
-        public static class ImplementationForWindowsBecauseGodForbidWeOmitTheDllExtensionInTheFuckingFilenameFuckYouMicrosoft
-        {
-            [System.Runtime.InteropServices.DllImport("PlexNative.dll")]
-            public static extern long MeasureString(string text, int textlen, string typeface, int typefacelen, double pointsize, int styles, int alignment, int wrapmode, int wrapwidth);
-
-            [System.Runtime.InteropServices.DllImport("PlexNative.dll")]
-            public static extern void DrawString(string text, int textlen, string typeface, int typefacelen, double pointsize, int styles, int alignment, int wrapmode, int wrapwidth, double r, double g, double b, double a, int w, int h, byte[] buffer);
-
-        }
-
         public NativeTextRenderer()
         {
             // Throw an exception if there was a DllImport failure so
             // that the engine can choose a different renderer.
-            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
-            {
-                System.Runtime.InteropServices.Marshal.PrelinkAll(typeof(ImplementationForWindowsBecauseGodForbidWeOmitTheDllExtensionInTheFuckingFilenameFuckYouMicrosoft));
-            }
-            else
-            {
-                System.Runtime.InteropServices.Marshal.PrelinkAll(typeof(Implementation));
-            }
+            System.Runtime.InteropServices.Marshal.PrelinkAll(typeof(Implementation));
         }
 		
 		public override Microsoft.Xna.Framework.Vector2 MeasureText(string text, System.Drawing.Font font, int maxwidth, Plex.Engine.GUI.TextAlignment alignment)
 		{
             long result = -1;
-            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
-            {
-                result  = ImplementationForWindowsBecauseGodForbidWeOmitTheDllExtensionInTheFuckingFilenameFuckYouMicrosoft.MeasureString(text, text.Length, font.FontFamily.Name, font.FontFamily.Name.Length, font.SizeInPoints, (int)font.Style, (int)alignment, (int)Implementation.WrapMode.Words, maxwidth);
-            }
-            else
-            {
-                result = Implementation.MeasureString(text, text.Length, font.FontFamily.Name, font.FontFamily.Name.Length, font.SizeInPoints, (int)font.Style, (int)alignment, (int)Implementation.WrapMode.Words, maxwidth);
-            }
+            result = Implementation.MeasureString(text, text.Length, font.FontFamily.Name, font.FontFamily.Name.Length, font.SizeInPoints, (int)font.Style, (int)alignment, (int)Implementation.WrapMode.Words, maxwidth);
             return new Microsoft.Xna.Framework.Vector2((int)(result & uint.MaxValue), (int)(result >> 32));
 		}
 		
@@ -68,14 +43,7 @@ namespace Plex.Engine.TextRenderers
 			var data = new byte[(int)measure.X * (int)measure.Y * 4];
 			if (data.Length == 0)
 				return;
-            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
-            {
-                ImplementationForWindowsBecauseGodForbidWeOmitTheDllExtensionInTheFuckingFilenameFuckYouMicrosoft.DrawString(text, text.Length, font.FontFamily.Name, font.FontFamily.Name.Length, font.SizeInPoints, (int)font.Style, (int)alignment, (int)Implementation.WrapMode.Words, maxwidth, color.B / 255.0, color.G / 255.0, color.R / 255.0, color.A / 255.0, (int)measure.X, (int)measure.Y, data);
-            }
-            else
-            {
-                Implementation.DrawString(text, text.Length, font.FontFamily.Name, font.FontFamily.Name.Length, font.SizeInPoints, (int)font.Style, (int)alignment, (int)Implementation.WrapMode.Words, maxwidth, color.B / 255.0, color.G / 255.0, color.R / 255.0, color.A / 255.0, (int)measure.X, (int)measure.Y, data);
-            }
+            Implementation.DrawString(text, text.Length, font.FontFamily.Name, font.FontFamily.Name.Length, font.SizeInPoints, (int)font.Style, (int)alignment, (int)Implementation.WrapMode.Words, maxwidth, color.B / 255.0, color.G / 255.0, color.R / 255.0, color.A / 255.0, (int)measure.X, (int)measure.Y, data);
             var tex2 = new Microsoft.Xna.Framework.Graphics.Texture2D(gfx.Device, (int)measure.X, (int)measure.Y);
 			tex2.SetData<byte>(data);
 			gfx.DrawRectangle(x, y, (int)measure.X, (int)measure.Y, tex2);
