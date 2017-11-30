484684c314967b3c15eb1ff38e5b97d626b42966
WatercolorGames <theplexgate@gmail.com>
2017-11-29 21:50:33 -0500

    Add pictureboxes...slightly. They don't really...work.

--- Peacenet/Applications/GameSettings.cs
+++ Peacenet/Applications/GameSettings.cs
@@ -7,12 +7,18 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Plex.Engine.GraphicsSubsystem;
+using Plex.Engine;
 
 namespace Peacenet.Applications
 {
     public class GameSettings : Window
     {
+        [Dependency]
+        private Plexgate _plexgate = null;
+
+
         private Label _test = null;
+        private PictureBox _picture = new PictureBox();
 
         public GameSettings(WindowSystem _winsys) : base(_winsys)
         {
@@ -25,13 +31,17 @@ namespace Peacenet.Applications
             _test.FontStyle = Plex.Engine.Themes.TextFontStyle.Header1;
             _test.Text = "Settings";
             AddChild(_test);
-            
+            AddChild(_picture);
+            _picture.AutoSize = true;
+            _picture.Texture = _plexgate.Content.Load<Texture2D>("Splash/Peacenet");
         }
 
         protected override void OnUpdate(GameTime time)
         {
             _test.X = 15;
             _test.Y = 15;
+            _picture.X = 15;
+            _picture.Y = _test.Y + _test.Height + 5;
             base.OnUpdate(time);
         }
     }
---
+++ ShiftOS_TheReturn/GUI/PictureBox.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Plex.Engine.GraphicsSubsystem;
+
+namespace Plex.Engine.GUI
+{
+    public class PictureBox : Control
+    {
+        private Texture2D _texture = null;
+        private Color _tint = Color.White;
+        private bool _autoSize = false;
+        private ImageLayout _layout = ImageLayout.Stretch;
+        private bool _premultiplied = false;
+
+        public Color Tint
+        {
+            get
+            {
+                return _tint;
+            }
+            set
+            {
+                if (_tint == value)
+                    return;
+                _tint = value;
+                Invalidate();
+            }
+        }
+
+        public bool AutoSize
+        {
+            get
+            {
+                return _autoSize;
+            }
+            set
+            {
+                if (_autoSize == value)
+                    return;
+                _autoSize = value;
+                Invalidate();
+            }
+        }
+
+        public bool Premultipied
+        {
+            get
+            {
+                return _premultiplied;
+            }
+            set
+            {
+                if (_premultiplied == value)
+                    return;
+                _premultiplied = value;
+                Invalidate();
+            }
+        }
+
+        public Texture2D Texture
+        {
+            get
+            {
+                return _texture;
+            }
+            set
+            {
+                if (_texture == value)
+                    return;
+                _texture = value;
+                Invalidate();
+            }
+        }
+
+        public ImageLayout Layout
+        {
+            get
+            {
+                return _layout;
+            }
+            set
+            {
+                if (_layout == value)
+                    return;
+                _layout = value;
+                Invalidate();
+            }
+        }
+
+        protected override void OnPaint(GameTime time, GraphicsContext gfx, RenderTarget2D currentTarget)
+        {
+            gfx.DrawRectangle(0, 0, Width, Height, _texture, _tint, _layout, false, _premultiplied);
+        }
+
+        protected override void OnUpdate(GameTime time)
+        {
+            if (AutoSize)
+            {
+                if (_texture != null)
+                {
+                    Width = _texture.Width;
+                    Height = _texture.Height;
+                }
+                else
+                {
+                    Width = 1;
+                    Height = 1;
+                }
+            }
+            base.OnUpdate(time);
+        }
+    }
+}
