97b52df9e1e261e4b59f242d50773714e125a817
WatercolorGames <theplexgate@gmail.com>
2017-12-15 19:57:19 -0500

    Single-instance

--- Peacenet/Program.cs
+++ Peacenet/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Plex.Engine;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Peacenet
 {
@@ -14,6 +16,15 @@ namespace Peacenet
         [STAThread]
         static void Main()
         {
+            var prc = Process.GetCurrentProcess();
+            var other = Process.GetProcesses().FirstOrDefault(x => x.ProcessName == prc.ProcessName && x.Id != prc.Id);
+            if(other != null)
+            {
+                System.Windows.Forms.Application.EnableVisualStyles();
+                System.Windows.Forms.MessageBox.Show(caption: "The Peacenet", text: "The Peacenet is already running on your system.", icon: System.Windows.Forms.MessageBoxIcon.Error, buttons: System.Windows.Forms.MessageBoxButtons.OK);
+                return;
+            }
+
             using (var game = new Plexgate())
                 game.Run();
         }
